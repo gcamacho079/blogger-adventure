a12de1ef9df6b7de241e80bed45c8c6ad65c98d4
Guadalupe Camacho <gcamacho079@gmail.com>
2017-02-20 09:55:04 -0800

    Begin work on edit method and template page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,8 +19,14 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  def delete
-    
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
 end
